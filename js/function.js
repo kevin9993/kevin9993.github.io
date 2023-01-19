b961338491a3e70e2ec81b604b637f245c5f6045
kevin9993 <122837866+kevin9993@users.noreply.github.com>
2023-01-19 19:08:45 -0300

    trabajo final casi terminado

---
+++ js/function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function (){
+console.log("capturamos el evento click");
+document.getElementById("demo").innerHTML = "Whatsapp: +543585017089 / Outlook: alfa-programa@hotmail.com"
+ }
