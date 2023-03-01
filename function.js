c11f641c4e3665e77a6fa2ed92a94d1b5e0dc8a2
kevin9993 <122837866+kevin9993@users.noreply.github.com>
2023-02-28 23:14:05 -0300

    nuevos cambios

---
+++ function.js
@@ -0,0 +1,21 @@
+
+
+
+function tocar(){
+    document.getElementById('info').style.display = 'block'
+}
+
+
+
+function abrir(){
+    document.getElementById('forma').style.display = 'block'
+}
+
+
+function ver(){
+    document.getElementById('exper').style.display = 'block'
+}
+
+function mostrar(){
+    document.getElementById('otros').style.display = 'block'
+}
